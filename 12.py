2d23497254f4b357e4f817f1efb6eb821005ddf4
Alex <1823748191@qq.com>
2023-09-13 08:19:57 +0800

    update 12.py

--- 12.py
+++ 12.py
@@ -1,12 +1,18 @@
 x=int(input())
-l=1
+judge=False
+if(x<0):
+    x=-x
+    judge=True
+l=0
 r=x
-while l<r:
-    mid=int((l+r)/2)
+while abs(r-l)>1e-5:
+    mid=(l+r)/2
     if (mid*mid*mid>x):
         r=mid
     else:
-        l=mid+1
-while(l*l*l>x):
-    l-=1
+        l=mid
+if(abs(round(l)-l)<1e-4):
+    l=round(l)
+if(judge):
+    l=-l
 print(l)
